44996efa4514fbdb5e06a95b523e41d064c869f7
Uri <=>
2022-01-01 08:02:04 +0200

    defualt

--- src/pages/HOME-b05io.js
+++ src/pages/HOME-b05io.js
@@ -1,9 +1,9 @@
-//Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction)
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 
 $w.onReady(function () {
 	// Write your Javascript code here using Velo framework
 
-	// Print hello world: console.log("Hello world!");
+	// “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
 
 	// Call functions on page elements: $w('#button1').label = "Click me!"
 });
