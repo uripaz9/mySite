8558b2c2328703fbd883a5708157376d26acb1ed
Uri <=>
2022-01-01 07:51:21 +0200

    defualt

--- src/pages/HOME-b05io.js
+++ src/pages/HOME-b05io.js
@@ -3,7 +3,7 @@
 $w.onReady(function () {
 	// Write your Javascript code here using Velo framework
 
-	// Print hello world: console.log("Hello world!"")
+	// Print hello world: console.log("Hello world!");
 
 	// Call functions on page elements: $w('#button1').label = "Click me!"
 });
